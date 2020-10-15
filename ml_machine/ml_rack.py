35f07dda071e1fad102d4f3d8d13d628801fafa2
mdkgo <pantwardybicek@gmail.com>
2020-10-15 15:17:42 +0200

    working updated 20201015

--- ml_machine/ml_rack.py
+++ ml_machine/ml_rack.py
@@ -10,14 +10,14 @@ class Rack ():
     def accept_classifiers (self, classifiers):
         self.final_classifier = classifiers[0]
         self.base_classifiers = [x for x in classifiers[1:]]
-        report({'estimators_base': [c.__name__ for c in self.base_classifiers],
+        report({'estimators':{'estimators_base': ','.join([c.__name__ for c in self.base_classifiers]),
                'estimators_final': self.final_classifier.__name__,
-               'rack_status': 'accepted classifiers'})
+               'rack_status': 'accepted classifiers'}})
 
     def deploy (self, rack_parameters: list):
         final_parameters = rack_parameters[0]
-        report({'estimators_params_base': rack_parameters[1:],
-                'estimators_params_final': rack_parameters[0]})
+        report({'parameters:':{'estimators_params_base': rack_parameters[1:],
+                'estimators_params_final': rack_parameters[0]}})
         self.final_rack = [self.deploy_estimator(self.final_classifier, **final_parameters)]
         base_parameters = rack_parameters[1:]
         self.base_rack = [
@@ -40,12 +40,13 @@ class Rack ():
 
     def base_predict(self,X):
         report({'rack_status': 'base_rack predicting'})
+        n_base_estimators = len(self.base_rack_fitted)
         if self.M.setup['baserack_trainset_split'] == False:
-            return [estimator.predict(X) for estimator in self.base_rack_fitted]
+            return np.array([estimator.predict(X) for estimator in self.base_rack_fitted]).reshape(-1,n_base_estimators)
         elif type(self.M.setup['baserack_trainset_split']) == tuple:
             report({'rack_status': 'fitting base estimators - trainset split'})
             split_X = [X[:, a:b] for (a, b) in self.M.setup['baserack_trainset_split']]
-            return np.array([pair[0].predict(pair[1]) for pair in zip(self.base_rack_fitted, split_X)]).T
+            return np.array([pair[0].predict(pair[1]) for pair in zip(self.base_rack_fitted, split_X)]).reshape(-1,n_base_estimators)
 
     def final_fit(self,X,y):
         report({'rack_status': 'fitting final estimator'})
